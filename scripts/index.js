9ff0d10549cfa90f74c9cf1bad0adf7eaf7b9f22
Rita Poliakova <iambublinbrook@yandex.ru>
2025-02-12 00:22:11 +0300

    Добавлена функция создания карточки

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,36 @@
 // @todo: Темплейт карточки
 
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function addCard(cardData, deleteCallback) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const cardDeleteButton = cardElement.querySelector('.card__delete-button');
+  const cardDescription = cardElement.querySelector('.card__description');
+  const cardLikeButton = cardElement.querySelector('.card__like-button');
+
+  cardImage.src = cardData.link;
+  cardTitle.textContent = cardData.name;
+
+  cardDeleteButton.addEventListener('click', () => {
+    deleteCallback(cardElement);
+  })
+
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((cardData) => {
+  const cardElement = addCard(cardData, deleteCard);
+  placesList.append(cardElement);
+});
